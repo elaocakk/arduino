2051523dbce2b2967b604bbb0bf4f4dc0d450a2e
elaocak <49361580+darkbluepebble@users.noreply.github.com>
2019-07-07 01:30:05 +0300

    Update dht11_sicaklik.ino

--- dht11_sicaklik/dht11_sicaklik.ino
+++ dht11_sicaklik/dht11_sicaklik.ino
@@ -1,28 +1,24 @@
-// https://www.brainy-bits.com/dht11-tutorial/
-
 #include "dht.h"
 #define dht_apin A0 
  
 dht DHT;
  
-void setup(){
- 
+void setup(){ 
   Serial.begin(9600);
-  delay(500);//Delay to let system boot
-  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
-  delay(1000);//Wait before accessing Sensor 
+  delay(500);
+  Serial.println("DHT11 Sicaklik Ve Nem Sensoru\n\n");
+  delay(1000);
 }
  
 void loop(){
     DHT.read11(dht_apin);
-    
-    Serial.print("Current humidity = ");
+   
+    Serial.print("Mevcut Nem = ");
     Serial.print(DHT.humidity);
     Serial.print("%  ");
-    Serial.print("temperature = ");
+    Serial.print("Sicaklik = ");
     Serial.print(DHT.temperature); 
     Serial.println("C  ");
     
     delay(5000);
- 
 }
